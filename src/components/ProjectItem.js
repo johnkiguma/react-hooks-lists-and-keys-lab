1c8c4341322a8e567c04a9a5acb02373956f6610
johnkiguma <154498415+johnkiguma@users.noreply.github.com>
2024-01-31 09:58:33 +0300

    completed assignment

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,4 +1,6 @@
+import { render } from "@testing-library/react";
 import React from "react";
+import ProjectList from "./ProjectList";
 
 function ProjectItem({ name, about, technologies }) {
   return (
@@ -6,7 +8,14 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+       <span>Rails</span>
+
+        <span>Bootstrap CSS</span>
+
+        <span>React</span>
+
+        <span>redux</span>
+
       </div>
     </div>
   );
